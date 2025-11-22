4396aaa4a02cc871c85964d95f6fc9f04a2bc3e9
Sean Rachmani <sicsicrr11@gmail.com>
2025-11-22 18:23:14 +0000

    finished phase 3

--- include/PointerWrapper.h
+++ include/PointerWrapper.h
@@ -37,9 +37,18 @@ public:
      * Think about ownership and resource management.
      * Is the default destructor sufficient here?
      */
-    ~PointerWrapper() =default;
+    //as we learned at lecture, we must override default destructor if we have pointer fields
+    ~PointerWrapper() {
+        delete ptr;
+    }
 
     // ========== COPY OPERATIONS (DELETED) ==========
+
+    /**
+     * as we can see no memory was allocated. only deleted.
+     * i didnt understand it completely yet but it seems that it happens only from outside the wrapper.\
+     * need to check.
+     */
 
     /**
      * Copy constructor is DELETED
@@ -50,6 +59,7 @@ public:
     /**
      * Copy assignment is DELETED
      * Consider what problems could arise if copying was allowed
+     * 
      */
     PointerWrapper& operator=(const PointerWrapper& other) = delete;
 
@@ -60,7 +70,10 @@ public:
      * HINT: How should ownership transfer from one wrapper to another?
      * What should happen to the source wrapper after the move?
      */
-    PointerWrapper(PointerWrapper&& other) noexcept {}
+    PointerWrapper(PointerWrapper&& other) noexcept {
+        ptr=other.ptr;
+        other.ptr=nullptr;
+    }
 
     /**
      * TODO: Implement move assignment operator
@@ -68,6 +81,11 @@ public:
      * Don't forget about self-assignment!
      */
     PointerWrapper& operator=(PointerWrapper&& other) noexcept {
+        if(&other!=this){
+            delete ptr;
+            ptr=other.ptr;
+            other.ptr=nullptr;
+        }
         return *this;
     }
 
@@ -80,17 +98,29 @@ public:
      */
 
     T& operator*() const {
+        if(!ptr){
+            throw std::runtime_error("null");
+        }
         return *ptr;
-    };
+    }
+
+
 
     /**
      * TODO: Implement arrow operator
      * HINT: How do you access members of the wrapped object?
      * What safety checks should you perform?
+     * 
+     *
      */
     T* operator->() const {
-        return nullptr;
+        if(!ptr){
+         throw std::runtime_error("null");
+        }
+        return ptr;
     }
+
+
 
     /**
      * TODO: Implement get() function
@@ -99,7 +129,10 @@ public:
      * @throws std::runtime_error if ptr is null
      */
     T* get() const {
-        return nullptr; // Placeholder
+        if(!ptr){
+         throw std::runtime_error("null");
+        } 
+        return ptr;
     }
 
     // ========== OWNERSHIP MANAGEMENT ==========
@@ -110,7 +143,9 @@ public:
      * Should the wrapper still own the pointer after calling release()?
      */
     T* release() {
-        return nullptr;
+        T* copy = ptr;
+        ptr = nullptr; //now the wrapper is empty
+        return copy;
     }
 
     /**
@@ -119,6 +154,8 @@ public:
      * What should happen to the old pointer?
      */
     void reset(T* new_ptr = nullptr) {
+        delete ptr;
+        ptr = new_ptr;
     }
 
     // ========== UTILITY FUNCTIONS ==========
@@ -128,8 +165,15 @@ public:
      * HINT: When should a wrapper be considered "true" or "false"?
      * Why might the explicit keyword be important here?
      */
+
+     //explicit no auto casting
+     //const no change variable
+     //no except no exception throw
     explicit operator bool() const {
-        return false; //placeholder
+        if(!ptr){
+            return false;
+        }
+        return true; 
     }
 
     /**
@@ -162,7 +206,7 @@ template<typename T>
 void swap(PointerWrapper<T>& lhs, PointerWrapper<T>& rhs) noexcept {
     // TODO: Implement global swap function
     // HINT: You can use the member swap function
-    //your code here...
+    lhs.swap(rhs);
 }
 
 #endif // POINTERWRAPPER_H
